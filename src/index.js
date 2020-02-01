bf8539d87a6afa78a829e768ec4c8265ec15f768
myexistance <joonyoonlives@gmail.com>
2020-02-02 04:07:31 +1300

    changing the shedule by using setState() on the button

--- src/index.js
+++ src/index.js
@@ -14,12 +14,41 @@ class App extends Component {
       <React.Fragment>
       <MDBContainer>
       <MDBRow>
-        <MDBCol lg="6">Left column</MDBCol>
-        <MDBCol lg="6">Right column</MDBCol>
+        <MDBCol md="8">
+        <Event title="Meeting with John" time="10:00" />
+        <Event title="Meeting with Joon" time="10:00"/>
+        </MDBCol>
+        <MDBCol md="4">Right column</MDBCol>
         </MDBRow>
       </MDBContainer>
       </React.Fragment>
     );
     }
 }
+
+class Event extends Component {
+    state = {
+      time: "10:00",
+      title: "My state name"
+    };
+    render() {
+    return (
+      <React.Fragment>
+        {/* This is genarated from state */}
+        <h3>
+          State:
+          {this.state.time} - {this.state.title}
+          <button
+            onClick={() => {
+              this.setState({ title: "Me NEW state title" });
+              console.log(this.state.title);
+            }}
+          >
+            Change state title
+          </button>
+        </h3>
+      </React.Fragment>
+    );
+  }
+  }
 ReactDOM.render(<App />, document.getElementById("root"));
